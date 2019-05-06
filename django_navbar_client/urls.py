544546e452507459b693f9c8af358ed5664f68c4
josu.bermudez <josu.bermudez@deusto.es>
2019-05-06 14:52:30 +0200

    URLs updated

--- django_navbar_client/urls.py
+++ django_navbar_client/urls.py
@@ -7,6 +7,6 @@ app_name = "django_navbar_client"
 urlpatterns = [
     path('api/login_callback/', oauth_callback, name="callback"),
     path('view/logout/', oauth_logout, name="logout"),
-    path('view/login/', oauth_login, name="logout"),
+    path('view/login/', oauth_login, name="login"),
     path('api/navbar/', oauth_navbar, name="navbar"),
 ]
